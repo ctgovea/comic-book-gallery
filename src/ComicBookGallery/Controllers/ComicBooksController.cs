27a14b7d45c1b57b72b93ea1c8e359d31ba7134e
Carlos Torres <ctgovea@gmail.com>
2016-07-25 22:06:52 -0500

    Added comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -15,7 +15,7 @@ namespace ComicBookGallery.Controllers
             {
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
                 {
                     new Artist() { Name = "Dan Slott", Role = "Script" },
@@ -27,7 +27,7 @@ namespace ComicBookGallery.Controllers
             };
 
             //ViewBag.ComicBook = comicBook;
-            
+
             return View(comicBook);
         }
 
